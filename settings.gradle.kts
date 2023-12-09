26d02e52e948b5803d4f5c4f58429d1b0667677e
Duolikun-Daniya <daniya.d@northeastern.edu>
2023-12-09 18:08:18 -0500

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "group-project-group-duolikun-daniya"
+include(":app")
+ 
