5af33ce1bc6c996163aa28e431aac9335f050e3e
Duolikun-Daniya <daniya.d@northeastern.edu>
2023-12-09 22:00:12 -0500

    Designed activity_sign_up.xml

--- app/src/main/java/edu/northeastern/group_project_group_duolikun_daniya/Sign_up.java
+++ app/src/main/java/edu/northeastern/group_project_group_duolikun_daniya/Sign_up.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Registration extends AppCompatActivity {
+public class Sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.activity_sign_up);
     }
 }
