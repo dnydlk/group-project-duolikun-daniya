5b3df74aa4bc6fc84e277d2795d22ff521d7f17b
Duolikun-Daniya <daniya.d@northeastern.edu>
2023-12-09 23:15:52 -0500

    Added log in option for activity_sign_up.xml

--- app/src/main/java/edu/northeastern/group_project_group_duolikun_daniya/LogIn.java
+++ app/src/main/java/edu/northeastern/group_project_group_duolikun_daniya/LogIn.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/edu/northeastern/group_project_group_duolikun_daniya/SignUp.java
+++ app/src/main/java/edu/northeastern/group_project_group_duolikun_daniya/SignUp.java
@@ -2,11 +2,13 @@ package edu.northeastern.group_project_group_duolikun_daniya;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,13 +20,14 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Sign_up extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     TextInputEditText editTextEmail;
     TextInputEditText editTextPassword;
     Button signUpBtn;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class Sign_up extends AppCompatActivity {
         editTextPassword = findViewById(R.id.password);
         progressBar = findViewById(R.id.progress_bar);
         signUpBtn = findViewById(R.id.sign_up_btn);
+        textView = findViewById(R.id.log_in_now);
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,12 +51,12 @@ public class Sign_up extends AppCompatActivity {
 
                 // Check if email or password is empty
                 if (email.isEmpty()) {
-                    Toast.makeText(Sign_up.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUp.this, "Enter email", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (password.isEmpty()) {
-                    Toast.makeText(Sign_up.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUp.this, "Enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -68,16 +72,25 @@ public class Sign_up extends AppCompatActivity {
 //                                    Log.d(TAG, "createUserWithEmail:success");
 //                                    FirebaseUser user = mAuth.getCurrentUser();
                                     Log.d(TAG, "createUserWithEmail:success");
-                                    Toast.makeText(Sign_up.this, "Account created.",
+                                    Toast.makeText(SignUp.this, "Account created.",
                                             Toast.LENGTH_SHORT).show();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(Sign_up.this, "Authentication failed.",
+                                    Toast.makeText(SignUp.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
