3700d287fb14f4e72e9bab72979c3afa7cdaeb10
Duolikun-Daniya <daniya.d@northeastern.edu>
2023-12-09 19:22:45 -0500

    Added Realtime Database library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,13 @@ dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
 
-    // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
 
     // Add the dependencies for any other desired Firebase products
     // https://firebase.google.com/docs/android/setup#available-libraries
+
+    // Add the dependency for the Realtime Database library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-database")
 }
